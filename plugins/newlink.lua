f5519b0c53166271ad26bab8faa9e0b019600760
ashkan-dev <ashkanpiri2@gmail.com>
2017-01-27 21:21:49 +0330

    Create newlink.lua

---
+++ plugins/newlink.lua
@@ -0,0 +1,25 @@
+if ( matches[1] == 'newlink' or matches[1]:lower() == "لینک جدید") and is_momod(msg)then
+			local function callback_link (extra , success, result)
+			local receiver = get_receiver(msg)
+				if success == 0 then
+					send_large_msg(receiver, '*Error: Failed to retrieve link* \nReason: Not creator.\n\nIf you have the link, please use /setlink to set it')
+					data[tostring(msg.to.id)]['settings']['set_link'] = nil
+					save_data(_config.moderation.data, data)
+				else
+					send_large_msg(receiver, "Created a new link")
+					data[tostring(msg.to.id)]['settings']['set_link'] = result
+					save_data(_config.moderation.data, data)
+				end
+			end
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] attempted to create a new SuperGroup link")
+			export_channel_link(receiver, callback_link, false)
+		end 
+    return {
+  patterns = {
+  "^[#!/]([Nn]ewlink)$",
+  "(لینک جدید)$",
+  "^!!tgservice (.+)$",
+  },
+  run = run,
+  pre_process = pre_process
+}
