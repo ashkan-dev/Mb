c8e7f3420d38f52dc92acd5a4c116c013fa3d7e3
ashkan-dev <ashkanpiri2@gmail.com>
2017-01-29 22:38:21 +0330

    Update newlink.lua

--- plugins/newlink.lua
+++ plugins/newlink.lua
@@ -1,24 +1,23 @@
-if matches[1] == 'newlink' and is_momod(msg)then
-			local function callback_link (extra , success, result)
-			local receiver = get_receiver(msg)
-				if success == 0 then
-					send_large_msg(receiver, '*Error: Failed to retrieve link* \nReason: Not creator.\n\nIf you have the link, please use /setlink to set it')
-					data[tostring(msg.to.id)]['settings']['set_link'] = nil
-					save_data(_config.moderation.data, data)
-				else
-					send_large_msg(receiver, "Created a new link")
-					data[tostring(msg.to.id)]['settings']['set_link'] = result
-					save_data(_config.moderation.data, data)
-				end
-			end
-			savelog(msg.to.id, name_log.." ["..msg.from.id.."] attempted to create a new SuperGroup link")
-			export_channel_link(receiver, callback_link, false)
-		end 
+   if matches[1] == 'newlink' and not is_realm(msg) then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local function callback (extra , success, result)
+        local receiver = 'chat#'..msg.to.id
+        if success == 0 then
+           return send_large_msg(receiver, '*Error: Invite link failed* \nReason: Not creator.')
+        end
+        send_large_msg(receiver, "Created a new link")
+        data[tostring(msg.to.id)]['settings']['set_link'] = result
+        save_data(_config.moderation.data, data)
+      end
+
     return {
   patterns = {
   "^[#!/]([Nn]ewlink)$",
-  "^!!tgservice (.+)$",
   },
   run = run,
-  pre_process = pre_process
+  
 }
+
+	
